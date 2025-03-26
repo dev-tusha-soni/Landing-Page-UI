1acfbae0a416e27aec7ee8bce5a82ac7827a1070
dev-tusha-soni <sonitushar464@gmail.com>
2025-03-26 23:57:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import ProfilePage from './pages/ProfilePage';
+import PaginationNavigation from './components/PaginationNavigation';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      <PaginationNavigation />
+    </Router>
+  );
+}
+
+export default App;
