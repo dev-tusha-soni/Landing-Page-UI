ee97d174d38893631241dbde9c4e5d9e8a523403
dev-tusha-soni <sonitushar464@gmail.com>
2025-03-27 12:38:34 +0530

    removed app.test.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/your-repo-name">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
